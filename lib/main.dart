8eda7277e2e06006132dc11c1830a32b89024ad4
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-01-22 10:33:33 +0900

    Add: flutter widget kit library

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,95 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_widgetkit/flutter_widgetkit.dart';
+import 'package:ios_widget_sample/flutter_widget_data.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    WidgetKit.reloadAllTimelines();
+    WidgetKit.reloadTimelines('test');
+
+    final data = FlutterWidgetData('Hello From Flutter');
+    final resultString =
+    await WidgetKit.getItem('testString', 'group.com.fasky');
+    final resultBool = await WidgetKit.getItem('testBool', 'group.com.fasky');
+    final resultNumber =
+    await WidgetKit.getItem('testNumber', 'group.com.fasky');
+    final resultJsonString =
+    await WidgetKit.getItem('testJson', 'group.com.fasky');
+
+    var resultData;
+    if (resultJsonString != null) {
+      resultData = FlutterWidgetData.fromJson(jsonDecode(resultJsonString));
+    }
+
+    WidgetKit.setItem('testString', 'Hello World', 'group.com.fasky');
+    WidgetKit.setItem('testBool', false, 'group.com.fasky');
+    WidgetKit.setItem('testNumber', 10, 'group.com.fasky');
+    WidgetKit.setItem('testJson', jsonEncode(data), 'group.com.fasky');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('iOS Widget Showcase'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Enter a text',
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+                child: TextField(controller: textController),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  WidgetKit.setItem(
+                    'widgetData',
+                    jsonEncode(
+                      FlutterWidgetData(textController.text),
+                    ),
+                    'group.com.fasky',
+                  );
+                  WidgetKit.reloadAllTimelines();
+                },
+                child: Text('Update Widget'),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  WidgetKit.removeItem('widgetData', 'group.com.fasky');
+                  WidgetKit.reloadAllTimelines();
+                },
+                child: Text('Remove Widget Data'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
